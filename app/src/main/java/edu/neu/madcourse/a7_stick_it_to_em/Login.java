0579c7a328dabaeeb474e7734b56113624ffc87c
zengxuan <zeng.xu@northeastern.edu>
2022-03-25 13:14:48 -0700

    changed launch page

--- app/src/main/java/edu/neu/madcourse/a7_stick_it_to_em/Login.java
+++ app/src/main/java/edu/neu/madcourse/a7_stick_it_to_em/Login.java
@@ -2,9 +2,12 @@ package edu.neu.madcourse.a7_stick_it_to_em;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     EditText input;
@@ -16,5 +19,14 @@ public class Login extends AppCompatActivity {
 
         input = findViewById(R.id.username);
         btn = findViewById(R.id.login);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, input.getText(),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
