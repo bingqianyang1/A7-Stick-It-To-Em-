8243cc513633d141d6d73ae772b49388ee0a445d
Jinyin Chai <cjy990324>
2022-03-25 15:20:17 -0700

    send sticker activity UI and click on stickers response test

--- app/src/main/java/edu/neu/madcourse/a7_stick_it_to_em/SendStickerActivity.java
+++ app/src/main/java/edu/neu/madcourse/a7_stick_it_to_em/SendStickerActivity.java
@@ -2,13 +2,39 @@ package edu.neu.madcourse.a7_stick_it_to_em;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SendStickerActivity extends AppCompatActivity {
+    private int selected_Sticker = 0;
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_sticker);
+        test = findViewById(R.id.test);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void clickImg(View view) {
+        switch (view.getId()) {
+            case R.id.sticker1:
+                selected_Sticker = R.drawable.sticker1;
+                test.setText("111");
+                break;
+            case R.id.sticker2:
+                selected_Sticker = R.drawable.sticker2;
+                break;
+            case R.id.sticker3:
+                selected_Sticker = R.drawable.sticker3;
+                break;
+            case R.id.sticker4:
+                selected_Sticker = R.drawable.sticker4;
+                break;
+        }
     }
 }
