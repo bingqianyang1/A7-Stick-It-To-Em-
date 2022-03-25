f536b0a20dcba5daa74a6b37649ecf3271c1167a
Jinyin Chai <cjy990324>
2022-03-25 13:14:49 -0700

    send sticker activity UI

---
+++ app/src/main/java/edu/neu/madcourse/a7_stick_it_to_em/SendStickerActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.a7_stick_it_to_em;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendStickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_sticker);
+    }
+}
